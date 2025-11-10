0aa3a872b4724fc1afbfb3af6f5e137195439695
Balaji <137058591+balaji20itl004@users.noreply.github.com>
2025-11-10 16:22:10 +0530

    Delete newfile.bat
    unexpected file

--- newfile.bat
+++
@@ -1,6 +0,0 @@
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/balaji20itl004/Trending-hashtag-tracker.git
-git push -u origin main
